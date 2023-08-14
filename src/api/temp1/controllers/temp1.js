a6ce4ea0f6d95f0a43efb692dcc02434e0511707
nikhileshr-lanatus <nikhilesh.r@lanatussystems.com>
2023-08-15 00:39:27 +0530

    initial4

---
+++ src/api/temp1/controllers/temp1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * temp1 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::temp1.temp1');
---
+++ src/api/temp1/services/temp1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * temp1 service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::temp1.temp1');
